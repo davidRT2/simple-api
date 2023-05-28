9671f1fd635d86fa1660af4638c4b348c0e0b523
Akbardavid <akbardavid034@gmail.com>
2023-05-28 21:10:40 +0700

    Update pada client web view

--- clientView/view.php
+++ clientView/view.php
@@ -10,26 +10,39 @@
 
 <body>
     <h1 id="response-header">Menunggu Response</h1>
-    <button id="get-data-button">Get Data</button>
+    <input type="text" id="input-field" placeholder="masukan nama"><br>
+    <button id="get-data-button">Hit Api</button>
 
     <script>
         // Mendapatkan referensi elemen tombol dan header
         var button = document.getElementById("get-data-button");
+        var input = document.getElementById("input-field");
         var header = document.getElementById("response-header");
 
         // Menambahkan event listener saat tombol ditekan
         button.addEventListener("click", function() {
             // Mengirim permintaan GET API menggunakan fetch()
-            
+
             var xhr = new XMLHttpRequest();
-            xhr.open("GET", "http://localhost:8080/", true);
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    var response = xhr.responseText;
-                    // Lakukan sesuatu dengan respons yang diterima
-                    header.textContent = response;
-                }
-            };
+            if (!(input.value === "")) {
+                xhr.open("GET", "http://localhost:8080/" + input.value, true);
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        var response = xhr.responseText;
+                        // Lakukan sesuatu dengan respons yang diterima
+                        header.textContent = response;
+                    }
+                };
+            } else {
+                xhr.open("GET", "http://localhost:8080/", true);
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        var response = xhr.responseText;
+                        // Lakukan sesuatu dengan respons yang diterima
+                        header.textContent = response;
+                    }
+                };
+            }
             xhr.send();
         });
     </script>
